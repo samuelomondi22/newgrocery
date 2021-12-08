e1fa1ff4ca57c02d8c702c04591c39a9bbe0210f
samuelomondi22 <omondisam696@gmail.com>
2021-12-08 11:42:53 -0700

    search result

--- grocery/app/views.py
+++ grocery/app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import redirect, render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, UpdateView
 from .models import Grocery, Mall
 from django.db.models import Q
 from django.contrib import messages
+from django.conf import settings
 
 # Create your views here.
 
@@ -12,7 +13,7 @@ class HomePageView(TemplateView):
 class GroceryDetail(TemplateView):
     template_name = 'detail.html'
     
-class AddGrocery(TemplateView):
+class AddGrocery(UpdateView):
     template_name = 'add_grocery.html'
 
     # retrieve and check if the item_name exist if it does call the add mall function is not then this function below is called
@@ -25,12 +26,10 @@ class AddGrocery(TemplateView):
                 new_item_price = self.request.POST.get('item_price')
                 new_item_mall = self.request.POST.get('item_mall')
                 if Grocery.objects.filter(item_name=new_item_name).exists():
-                    grocery_to_update = Grocery.objects.filter(item_name = new_item_name)
-                    Mall.objects.filter(grocery=grocery_to_update, item_mall=new_item_mall).update(new_item_price=new_item_price)
+                    return redirect('')
                 else:
                     new_grocery = Grocery(item_name=new_item_name)
                     new_grocery.save()
-
                     # mall_set is built in function, saves the mall items with the accsiated new_grocery
                     new_grocery.mall_set.create(item_price=new_item_price, item_mall=new_item_mall, grocery=new_grocery)
                     new_grocery.save()
@@ -39,9 +38,9 @@ class AddGrocery(TemplateView):
                 # stores the message in the database
                 messages.success(self.request, "Item Added Successfully")
 
-                return redirect('/')
+                return redirect('add_grocery')
             else:
-                return redirect('/')
+                return redirect('add_grocery')
 
 
 class NoGrocery(TemplateView):
@@ -58,9 +57,7 @@ class SearchResultsView(ListView):
         if Grocery.objects.filter(item_name=query).exists():
             object_list = Grocery.objects.get(Q(item_name__icontains=query))
             return object_list
-        else:
-            messages.error(self.request,"Item does not exist in the database would you like to add?")
-            return redirect("search_results.html")
+     
     
     # This function allows you to post a mall, has nothing to do with groceries
     def post(self, request):
